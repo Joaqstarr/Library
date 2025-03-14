3914f84cd7fb08e4baddaf9859b39e3fb2a615ff
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-03-14 16:10:50 -0400

    Third Person Movement and camera

---
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    public class PlayerControls : MonoBehaviour
+    {
+        public Vector2 MoveInput { get; private set; }
+        public Vector2 LookInput { get; private set; }
+
+        public void OnMove(InputValue value)
+        {
+            MoveInput = value.Get<Vector2>();
+        }
+
+        public void OnLook(InputValue value)
+        {
+            LookInput = value.Get<Vector2>();
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 6.0f;
+        [SerializeField] private float _turnSmoothTime = 0.1f;
+        private float _turnSmoothVelocity;
+
+        private CharacterController _characterController;
+        private PlayerControls _playerControls;
+        private Transform _cameraTransform;
+
+        private void Awake()
+        {
+            _characterController = GetComponent<CharacterController>();
+            _playerControls = GetComponent<PlayerControls>();
+            _cameraTransform = Camera.main.transform;
+        }
+
+        private void Update()
+        {
+            Vector2 moveInput = _playerControls.MoveInput;
+            Vector3 direction = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
+
+            if (direction.magnitude >= 0.1f)
+            {
+                // Calculate the direction relative to the camera
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _cameraTransform.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                _characterController.Move(moveDirection * _speed * Time.deltaTime);
+            }
+        }
+    }
+}
