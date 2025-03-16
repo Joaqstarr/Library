e493a6bfcd6920d2a0dc29a4ffd5af3e59e75e45
Joaquin Royer <joaqstarr@gmail.com>
2025-03-16 18:48:10 -0400

    Pipe Shader work

---
+++ Assets/Scripts/Level/Pipe/PipeLump.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Splines;
+
+namespace Level.Pipe
+{
+    [RequireComponent(typeof(SplineContainer))]
+    public class PipeLump : MonoBehaviour
+    {
+        [SerializeField, Range(0,1)] 
+        public float _lumpPosition = 0;
+        public Vector3 LumpPosition { get; private set; }
+        private SplineContainer _splineContainer;
+        
+        private void Awake()
+        {
+            _splineContainer = GetComponent<SplineContainer>();
+        }
+
+        private void Update()
+        {
+            Vector3 pos = _splineContainer.Spline.EvaluatePosition(_lumpPosition);
+
+            LumpPosition = transform.TransformPoint(pos);
+            
+            
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (_splineContainer == null)
+            {
+                _splineContainer = GetComponent<SplineContainer>();
+
+            }
+            
+            
+            Vector3 pos = _splineContainer.Spline.EvaluatePosition(_lumpPosition);
+            pos = transform.TransformPoint(pos);
+            
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(pos, 0.2f);
+        }
+    }
+}
---
+++ Assets/Scripts/Level/Pipe/PipePart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Level.Pipe
+{
+    public class PipePart : MonoBehaviour
+    {
+        [SerializeField]
+        private MeshRenderer _meshRenderer;
+        private PipeLump _pipeLump;
+        private void Start()
+        {
+            _pipeLump = transform.parent.GetComponent<PipeLump>();
+        }
+
+        private void Update()
+        {
+            if (_pipeLump && _meshRenderer)
+            {
+                _meshRenderer.material.SetVector("_LumpPosition", _pipeLump.LumpPosition);
+            }
+        }
+    }
+}
