a3664e53604564cb4a892e4ef1fcdd0a96e618a6
Joaquin Royer <joaqstarr@gmail.com>
2025-03-18 13:18:22 -0400

    Removed unused variable from pipe part

--- Assets/Scripts/Level/Pipe/PipePart.cs
+++ Assets/Scripts/Level/Pipe/PipePart.cs
@@ -8,14 +8,10 @@ namespace Level.Pipe
         [SerializeField]
         private MeshRenderer _meshRenderer;
 
-        private MeshFilter _meshFilter;
         private PipeLump _pipeLump;
         private void Start()
         {
             _pipeLump = transform.GetComponentInParent<PipeLump>();
-            
-            if(_meshRenderer)
-                _meshFilter = _meshRenderer.gameObject.GetComponent<MeshFilter>();
         }
 
         private void Update()
