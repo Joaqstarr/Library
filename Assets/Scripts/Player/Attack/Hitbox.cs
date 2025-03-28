92eddf55e7d5f03538d71a4ab784f2803543ff7c
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-03-27 22:49:36 -0400

    Implement Attack Hitbox

---
+++ Assets/Scripts/Player/Attack/Hitbox.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Systems.Steam;
+using UnityEngine;
+
+namespace Player.Attack
+{
+    public class Hitbox : MonoBehaviour
+    {
+        public List<SteamResourceHolder> SteamHoldersInRange { get; private set; } = new List<SteamResourceHolder>();
+
+        private void OnTriggerEnter(Collider other)
+        {
+            SteamResourceHolder holder = other.GetComponent<SteamResourceHolder>();
+            if (holder != null)
+            {
+                SteamHoldersInRange.Add(holder);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            SteamResourceHolder holder = other.GetComponent<SteamResourceHolder>();
+            
+            if (holder != null)
+            {
+                SteamHoldersInRange.Remove(holder);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Attack/HitboxRotator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Cinemachine;
+using UnityEngine;
+
+namespace Player.Attack
+{
+    public class HitboxRotator : MonoBehaviour
+    {
+        [SerializeField]
+        private CinemachineVirtualCameraBase _thirdPersonCamera;
+
+
+        private void Update()
+        {
+
+            if (CinemachineCore.Instance.IsLive(_thirdPersonCamera))
+            {
+                transform.forward = Camera.main.transform.forward;
+            }
+            else
+            {
+                transform.forward = transform.parent.forward;
+            }
+        }
+    }
+}
