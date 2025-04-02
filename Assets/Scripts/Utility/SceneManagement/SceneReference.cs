f2acb61ee604f98ab1fee8e6c7e7a864269b9d95
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-04-02 08:45:42 -0400

    Create Scene Reference Scriptable Object

---
+++ Assets/Scripts/Utility/SceneManagement/SceneReference.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Utility.SceneManagement
+{
+    [CreateAssetMenu(fileName = "Scene Reference", menuName = "Scene Reference", order = 0)]
+    public class SceneReference : ScriptableObject
+    {
+        [field: SerializeField]
+        public string ScenePath { get; private set; }
+
+        public bool IsLoaded()
+        {
+            return SceneManager.GetSceneByPath(ScenePath).isLoaded;
+        }
+        
+        public void LoadScene()
+        {
+            SceneManager.LoadSceneAsync(ScenePath, LoadSceneMode.Additive);
+        }
+        
+        public void UnloadScene()
+        {
+            SceneManager.UnloadSceneAsync(ScenePath, UnloadSceneOptions.None);
+        }
+    }
+}
