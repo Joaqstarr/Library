8b581e438c93327bf89977dc88a7cf8c978c056f
Joaquin Royer <joaqstarr@gmail.com>
2025-04-02 17:22:00 -0400

    Create core gameplay management scene

---
+++ Assets/Scripts/Utility/SceneManagement/SceneLoadOnStart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utility.SceneManagement
+{
+    public class SceneLoadOnStart : MonoBehaviour
+    {
+        [SerializeField]
+        private SceneReference _sceneReference;
+
+        private void Start()
+        {
+            _sceneReference.LoadScene();
+        }
+    }
+}
