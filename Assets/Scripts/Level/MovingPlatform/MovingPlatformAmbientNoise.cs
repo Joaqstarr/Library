42ad9af531e648806e1e66d1e512f4078cf1152e
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-04-06 15:18:33 -0400

    Implement Moving Platform SFX

---
+++ Assets/Scripts/Level/MovingPlatform/MovingPlatformAmbientNoise.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Level.MovingPlatform
+{
+    public class MovingPlatformAmbientNoise : MonoBehaviour
+    {
+        private MovingPlatformVelocityTransfer _movingPlatformBehavior;
+        private AudioSource _audioSource;
+        private void Awake()
+        {
+            _movingPlatformBehavior = GetComponent<MovingPlatformVelocityTransfer>();
+            _audioSource = GetComponent<AudioSource>();
+
+            _audioSource.time = Random.Range(0, _audioSource.clip.length);
+
+        }
+
+        private void Update()
+        {
+            float vel = _movingPlatformBehavior.CurrentDelta.magnitude / Time.deltaTime;
+
+            _audioSource.volume = vel;
+        }
+    }
+}
