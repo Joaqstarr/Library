461ce804879ebc067e064def93b4a50f64f230b9
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-04-08 12:08:52 -0400

    Implement Fan Art

---
+++ Assets/Scripts/Level/Launchpad/FanSpinner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Level.Launchpad
+{
+    public class FanSpinner : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _rotateSpeed;
+        
+        private void Update()
+        {
+            transform.Rotate(_rotateSpeed * Time.deltaTime);
+        }
+    }
+}
