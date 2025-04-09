608de1c574731e0f2ef672f000ab1d8437a6bcc9
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-04-08 22:33:10 -0400

    Main Menu Prototype Code

---
+++ Assets/Scripts/UI/PrototypeMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class PrototypeMenu : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            SceneManager.LoadScene("Archives");
+        }
+        
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
