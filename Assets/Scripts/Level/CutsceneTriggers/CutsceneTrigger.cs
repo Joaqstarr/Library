17554b35a74f684149d1c1984f61df4d1c2b6803
Joaquin Royer <joaqstarr@gmail.com>
2025-04-16 16:01:28 -0400

    Create cutscene trigger base

---
+++ Assets/Scripts/Level/CutsceneTriggers/CutsceneTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Systems.CutsceneSystem;
+using UnityEngine;
+
+namespace Level.CutsceneTriggers
+{
+    public class CutsceneTrigger : MonoBehaviour
+    {
+        [SerializeField]
+        private CutsceneData _cutsceneToPlay;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                if (CutsceneEventManager.instance)
+                {
+                    CutsceneEventManager.instance.PlayCutscene(_cutsceneToPlay);
+                }
+            }
+        }
+    }
+}
