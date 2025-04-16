5b00544b80798b0df124401b7a062fb8db3ae56b
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-04-16 09:36:13 -0400

    Create Cutscene Player

---
+++ Assets/Scripts/Systems/CutsceneSystem/CutsceneData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Timeline;
+
+namespace Systems.CutsceneSystem
+{
+    [CreateAssetMenu(fileName = "New Cutscene Data", menuName = "Cutscenes/Cutscene Data", order = 0)]
+    public class CutsceneData : ScriptableObject
+    {
+        [field: SerializeField]
+        public TimelineAsset Cutscene { get; private set; } 
+    }
+}
