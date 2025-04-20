f324c0144e6cf22eae761c358e07d0809ba544e7
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-04-20 18:03:57 -0400

    Implement Third Blob Launch animation

---
+++ Assets/Scripts/Utility/TransformRotationCopier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utility
+{
+    public class TransformRotationCopier : MonoBehaviour
+    {
+        [SerializeField]private Transform _transformToCopy;
+
+        private void Update()
+        {
+            transform.localRotation = _transformToCopy.localRotation;
+        }
+    }
+}
