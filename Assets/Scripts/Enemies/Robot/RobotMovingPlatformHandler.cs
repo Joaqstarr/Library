ddbafb6a0f43f2cb84844e679e385e3a80b574ae
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-04-23 22:11:58 -0400

    Friendly robot experimentation

---
+++ Assets/Scripts/Enemies/Robot/RobotMovingPlatformHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Level.MovingPlatform;
+using UnityEngine;
+
+namespace Enemies.Robot
+{
+    public class RobotMovingPlatformHandler : MonoBehaviour
+    {
+        private Rigidbody _enemyRb;
+        private MovingPlatformVelocityTransfer _currentPlatform;
+        private float _platformTimer = 0f;
+
+        [Tooltip("How long to stay attached to the platform after contact is lost.")]
+        public float graceTime = 0.1f;
+
+        private void Awake()
+        {
+            _enemyRb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            if (_currentPlatform != null)
+            {
+                _enemyRb.MovePosition(_currentPlatform.CurrentDelta);
+            }
+
+            
+            // Countdown grace timer
+            if (_platformTimer > 0f)
+            {
+                _platformTimer -= Time.deltaTime;
+                if (_platformTimer <= 0f)
+                {
+                    _currentPlatform = null;
+                }
+            }
+        }
+
+        private void OnCollisionStay(Collision other)
+        {
+            if (other.gameObject.CompareTag("MovingPlatform"))
+            {
+                _currentPlatform = other.gameObject.GetComponentInParent<MovingPlatformVelocityTransfer>();
+                _platformTimer = graceTime;
+            }
+        }
+
+
+    }
+}
