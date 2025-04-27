37313a9f0d7c87bc0738146e6edae3e92bb19f7a
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-04-27 16:18:10 -0400

    Reactive button prompts

--- Assets/Scripts/UI/InteractableUI.cs
+++ Assets/Scripts/UI/InteractableUI.cs
@@ -1,24 +1,42 @@
 ﻿using System;
+using System.Linq;
 using DG.Tweening;
 using Player;
 using Systems.Interaction;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
 
 namespace UI
 {
     public class InteractableUI : MonoBehaviour
     {
         private TMP_Text _text;
+
+        private InputDevice _currentDevice;
         private void Awake()
         {
             _text = GetComponent<TMP_Text>();
+
+        }
+        
+        private void OnAnyButtonPress(InputControl control)
+        {
+            // Handle the input event here
+            _currentDevice = control.device;
+
         }
 
         private void OnEnable()
         {
+            InputSystem.onAnyButtonPress.Call(OnAnyButtonPress);
             PlayerInteractionManager.OnClosestInteractableChanged += PlayerInteractionManagerOnOnClosestInteractableChanged;
         }
+
+
+
+
 
         private void OnDisable()
         {
@@ -33,10 +51,29 @@ namespace UI
                 return;
             }
 
+            //InputSystem.GetDevice()
             //transform.DOShakePosition(0.2f, 50, 5, 70);
 
             transform.DOPunchScale(Vector3.one * 0.3f, 0.15f, 6, 0f);
-            _text.text = "E to " + obj.InteractableName + obj.InteractableName;
+
+            string controlText;
+
+
+            if (_currentDevice is Gamepad)
+            {
+                controlText = "<font=\"promptfont SDF\">\u21a4</font>";
+            }
+            else if (_currentDevice is Keyboard || _currentDevice is Mouse)
+            {
+                controlText = "E";
+            }
+            else
+            {
+                controlText = "?"; // Fallback for unknown devices
+            }
+            _text.text = controlText + " to " + obj.InteractVerb + " " + obj.InteractableName;
+            
+            
         }
     }
 }
