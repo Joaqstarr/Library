26e1b5df5ff686481c4cc84856a509044121a8dc
IcedQ6 <157545403+IcedQ6@users.noreply.github.com>
2025-04-26 20:15:22 -0400

    Progress on Steam Wall
    Not very happy with how it looks just yet

--- Assets/Scripts/Level/SteamDoor/SteamDoor.cs
+++ Assets/Scripts/Level/SteamDoor/SteamDoor.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.VFX;
 
 public class SteamDoor : MonoBehaviour
 {
@@ -10,17 +11,26 @@ public class SteamDoor : MonoBehaviour
     [SerializeField]
     private GameObject pillar;
 
+    [SerializeField] private GameObject vfx;
+
+    void Start()
+    {
+        vfx.gameObject.GetComponent<VisualEffect>().SendEvent("OnPlay");
+    }
+
     public void disableWall()
     {
         steamWall.gameObject.GetComponent<MeshRenderer>().enabled = false;
         steamWall.gameObject.GetComponent<BoxCollider>().enabled = false;
         steamWall.gameObject.GetComponent<NavMeshObstacle>().enabled = false;
+        vfx.gameObject.GetComponent<VisualEffect>().SendEvent("OnStop");
     }
     public void enableWall()
     {
         steamWall.gameObject.GetComponent<MeshRenderer>().enabled = true;
         steamWall.gameObject.GetComponent<BoxCollider>().enabled = true;
         steamWall.gameObject.GetComponent<NavMeshObstacle>().enabled = true;
+        vfx.gameObject.GetComponent<VisualEffect>().SendEvent("OnPlay");
     }
 
 
