63bbf332e47f44014a8a28d0a55f924f59a8da79
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-04-26 23:34:40 -0400

    Implement Inhale/Exhale Icons

--- Assets/Scripts/UI/ActionStateDebugText.cs
+++ Assets/Scripts/UI/ActionStateDebugText.cs
@@ -1,23 +1,46 @@
 ﻿using System;
+using DG.Tweening;
 using Player.Attack;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class ActionStateDebugText : MonoBehaviour
     {
-        private TMP_Text _text;
+        [SerializeField] private Image _inhaleIcon;
+        [SerializeField] private Image _exhaleIcon;
 
+        [SerializeField] private Vector3 _scalePunchAmount;
+        [SerializeField] private float _scalePunchDuration;
+
+        [SerializeField] private float _shakeStrength = 1;
         private void Awake()
         {
-            _text = GetComponent<TMP_Text>();
+            UpdateState(true);
         }
 
         public void UpdateState(bool isInhaling)
         {
-            _text.text = isInhaling ? "Inhaling" : "Exhaling";
-            
+            if (isInhaling)
+            {
+                _inhaleIcon.color = Color.white;
+                _exhaleIcon.color = Color.clear;
+            }
+            else
+            {
+                _inhaleIcon.color = Color.clear;
+                _exhaleIcon.color = Color.white;
+            }
+
+            transform.DOComplete();
+            transform.DOShakePosition(_shakeStrength, _scalePunchDuration, 10);
+            transform.DOPunchScale(_scalePunchAmount, _scalePunchDuration, 1, 0.5f)
+                .OnComplete(() =>
+                {
+                    transform.localScale = Vector3.one;
+                });
         }
 
         private void OnEnable()
